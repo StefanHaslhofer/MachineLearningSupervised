18070b24bd98faf44e77fed2a460b288196f094a
shaslhofer <haslhofer@unisoftwareplus.com>
2024-01-10 22:26:12 +0100

    assigment 6

--- Assignment6/debug.py
+++ Assignment6/debug.py
@@ -1,220 +1,160 @@
-#NOTE#######################################################################################################
-#Please add all your imports in this cell only
-############################################################################################################
-
-#Nothing to do here
 import numpy as np
-import pandas as pd
-import sys
-import time
-import numpy as np
-import sklearn.ensemble
-from sklearn.ensemble import RandomForestClassifier
-from mnist_loader import MNIST
+from sklearn.metrics import roc_curve, auc
+from sklearn.utils import shuffle
+import matplotlib
 import matplotlib.pyplot as plt
-from matplotlib import style
-import matplotlib as mpl
-import seaborn as sns
-from sklearn.metrics import accuracy_score, confusion_matrix
-# Set random seed to ensure reproducible runs
-RSEED = 10
+from typing import Tuple
 
-#Load training and test data (routine from previous assignment)
-data = MNIST('./dataset/')
-img_train, labels_train = data.load_training()
-x_train = np.array(img_train)
-y_train = np.array(labels_train)
-x_test,y_test = data.load_testing()
-x_test = np.array(x_test)
-y_test = np.array(y_test)
-print(y_train)
-print(y_test)
+# set the seed for reproducability to 44
+RSEED = 44
+np.random.seed(RSEED)
 
-
-def _filter_(
-        x_train: np.ndarray,
-        y_train: np.ndarray,
-        x_test: np.ndarray,
-        y_test: np.ndarray,
-        labels_list: list[int, int]
-) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
-    """This function filters the datasets w.r.t to given labels_list.
-    So, in the end x_train and x_test only contain the samples with labels that are in labels_list and y_train and y_test only contain 2 labels (i.e. 0 and 1).
+def logistic_gradient(w: np.ndarray, x: np.ndarray, y: np.ndarray) -> np.ndarray:
+    """Function that computes the logistic gradient via the cross-entropy loss.
 
     Parameters
     ----------
-    x_train : np.ndarray
-        Training data matrix.
-    y_train : np.ndarray
-        Training labels vector.
-    x_test : np.ndarray
-        Test data matrix.
-    y_test : np.ndarray
-        Test labels vector.
-    labels_list : list[int, int]
-        list of length 2 which consists of integer labels.
+    w : np.ndarray
+        weights
+    x : np.ndarray
+        data matrix
+    y : np.ndarray
+        data labels
 
     Returns
     -------
-    tuple(np.ndarray, np.ndarray, np.ndarray, np.ndarray)
-        Returns the filtered train and test data matrix and labels vector (which only conists of 0s and 1s now).
-        Check the return statement to see the actual order of returned arrays.
+    np.ndarray
+        gradient vector
     """
-    # Your code goes here ↓↓↓
-    mask = [True if y in labels_list else False for y in y_train]
-    x_train_filtered = x_train[mask]
-    y_train_filtered = y_train[mask]
-    y_train_filtered = np.array([1 if y == 3 else 0 for y in y_train_filtered])
+    print(w)
+    gradient = np.zeros(shape=np.shape(w))
+    for i in range(len(w)):
+        gradient[i] = np.sum(((1 / (1 - np.exp(np.ndarray.transpose(w)[0] * x[i]))) - y[i]) * x[i])
 
-    mask = [True if y in labels_list else False for y in y_test]
-    x_test_filtered = x_test[mask]
-    y_test_filtered = y_test[mask]
-    y_test_filtered = np.array([1 if y == 3 else 0 for y in y_test_filtered])
-    # Your code ends here _____________________________________________________________________________________
-
-    return x_train_filtered, y_train_filtered, x_test_filtered, y_test_filtered
-
-x_train, y_train, x_test, y_test = _filter_(x_train, y_train, x_test, y_test, [3,8])
-print(y_train)
-print(y_test)
+    print(gradient)
+    return gradient
 
 
-def fit_predict(
-        x_train: np.ndarray,
-        y_train: np.ndarray,
-        x_test: np.ndarray,
-        y_test: np.ndarray,
-        rseed: int
-) -> tuple[RandomForestClassifier, np.ndarray]:
-    """Function fits a RandomForestClassifier on the training data and returns predictions as well as model.
+def generate_random(nr_samples: int, nr_features: int, seed=RSEED) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    """Function that generates a random matrix X and the random vectors y and weights according to input specifications.
+    Hint: to generate the distributions use numpy's np.random.<...> functions.
 
     Parameters
     ----------
-    x_train : np.ndarray
-        Training data matrix.
-    y_train : np.ndarray
-        Training labels vector.
-    x_test : np.ndarray
-        Test data matrix.
-    y_test : np.ndarray
-        Test labels vector.
-    rseed : int
-        Random Seed used for initializing the Classifier.
-
+    nr_samples : int,
+        number of samples to generate
+    nr_features : int,
+        number of features of each sample
+    seed: int, optional
+        seed for np.random, dont change it!
     Returns
     -------
-    tuple(RandomForestClassifier, np.ndarray)
-        Where the classifier is the already trained classifier and the array is the predictions on x_test.
+    tuple(np.ndarray,np.ndarray,np.ndarray)
+        returns randomly generated data matrices X,y and w.
     """
-    # Your code goes here ↓↓↓
-    clf = RandomForestClassifier(random_state=rseed)
-    model = clf.fit(x_train, y_train)
+    # don't change the seed!
+    np.random.seed(seed)
 
-    prediction = model.predict(x_test)
-    # Your code ends here _____________________________________________________________________________________
+    X_random = np.random.normal(size=(nr_features, nr_samples))
+    y_random = np.random.randint(size=(1, nr_samples), low=0, high=2)
+    w_random = np.random.normal(size=(nr_features, 1))
 
-    return model, prediction
-
-
-model, prediction = fit_predict(x_train,y_train,x_test,y_test, RSEED)
+    return (X_random, y_random, w_random)
 
 
-def get_n_items_wrong(
-        x_test:
-        np.ndarray, y_test:
-        np.ndarray,
-        prediction: np.ndarray
-) -> tuple[int, int, np.ndarray, np.ndarray, np.ndarray]:
-    """Calculates the size of the test set, number of misclassified samples, the samples that are misclassified,
-    the actual labels that are misclassified in the prediction, and the labels that are wrong in the prediction.
+def numerical_gradient(w: np.ndarray, x: np.ndarray, y: np.ndarray, eps_list: list, cost_function: callable) -> list:
+    """Function that computes the numerical gradient for each epsilon in eps_list.
+    Hint: Use the previously implemented cost function, it is given as input to this function,
+    make sure to call it with the correct name (cost_function(...), not cost(...))!
 
     Parameters
     ----------
-    x_test : np.ndarray
-        Test data matrix.
-    y_test : np.ndarray
-        Test labels vector.
-    prediction : np.ndarray
-        The predicted labels vector from the model.
+    w : np.ndarray
+        weights
+    x : np.ndarray
+        data matrix
+    y : np.ndarray
+        data labels
+    eps : list
+        list of floats; the epsilon values for difference quotient calculation
+    cost_function: callable
+        function that calculates the cross-entropy loss, implemented by you
 
     Returns
     -------
-    tuple(int, int, np.ndarray, np.ndarray, np.ndarray)
-        Where the first two ints are the size_test and num_wrong and the arrays are the samples_wrong, labels_wrong and predictions_wrong.
+    list
+        list of same length as eps_list containing the np.ndarrays from the computed gradients for each epsilon in eps_list
     """
+    dw_list = []
+
     # Your code goes here ↓↓↓
-    size_test = len(x_test)
-    # create mask via np.not_equal
-    mask = np.not_equal(y_test, prediction)
-    num_wrong = len(prediction[mask])
-    samples_wrong = x_test[mask]
-    labels_wrong = y_test[mask]
-    predictions_wrong = prediction[mask]
-    # Your code ends here _____________________________________________________________________________________
 
-    return size_test, num_wrong, samples_wrong, labels_wrong, predictions_wrong
-
-#SOLUTION TO NUMBER OF WRONG PREDICTIONS
-#Your code goes here ↓↓↓
-size_test, num_wrong, samples_wrong, labels_wrong, predictions_wrong = get_n_items_wrong(x_test, y_test, prediction)
-#Your code ends here _____________________________________________________________________________________
-
-#Following print statement might be evaluated
-print("Number of test samples: {0}\nNumber of misclassified samples: {1}".format(size_test,num_wrong))
+    return dw_list
 
 
-# PLOTTING HEATMAPS
-def plot_heatmaps(x_train: np.ndarray, y_train: np.ndarray, model: RandomForestClassifier) -> plt.Figure:
-    """Create plots as described in the task description above.
+def cost(w: np.ndarray, x: np.ndarray, y: np.ndarray) -> float:
+    """Function that computes the cross-entropy loss
 
     Parameters
     ----------
-    x_train : np.ndarray
-        Training data matrix.
-    y_train : np.ndarray
-        Training labels vector.
-    model : RandomForestClassifier
-        Your already trained classifier.
+    w : np.ndarray
+        weights
+    x : np.ndarray
+        data matrix
+    y : np.ndarray
+        data labels
 
     Returns
     -------
-    plt.Figure
-        A matplotlib.pyplot figure object (i.e. the heatmaps).
+    float
+        cross-entropy loss
     """
+    loss = None
     # Your code goes here ↓↓↓
 
-    # Use your freestlye plotting
+    return loss
 
-    # Step 1 Split and calculate averages
-    dress_av = np.reshape(np.mean(x_train[y_train == 1], axis=0), (28, 28))
-    bag_av = np.reshape(np.mean(x_train[y_train == 0], axis=0), (28, 28))
 
-    # Step 2 Subtract
-    diff = dress_av - bag_av
+def comparison(grad_a: np.ndarray, grad_n: np.ndarray) -> bool:
+    """Function that compares two arrays, in our case the numerical and analytical gradients.
+    Hint: use absolute tolerance, with an error tolerance of 1e-7.
 
-    # Step 3 Extract feature importance
-    importances = np.reshape(model.feature_importances_, (28, 28))
+    Parameters
+    ----------
+    grad_a : np.ndarray
+        the analytical gradient
+    grad_n : np.ndarray
+        the numberical gradient
 
-    # Step 4 plot everything together
-    fig, axs = plt.subplots(2,2,figsize=(14, 14), gridspec_kw = {'wspace':0.15,'hspace':0.3})
-    # bag
-    sns.heatmap(bag_av, ax=axs[0, 0])
-    axs[0, 0].set_title('Average bags')
+    Returns
+    -------
+    bool
+        True, if arrays are close, according to our specifications, False if not.
+    """
+    close = np.all(np.abs(grad_a - grad_n) <= 1e-7)
 
-    # dress
-    sns.heatmap(dress_av, ax=axs[0, 1])
-    axs[0, 1].set_title('Average dresses')
+    return close
 
-    # diff
-    sns.heatmap(diff, ax=axs[1, 0])
-    axs[1, 0].set_title('Overlaps')
+#Nothing to do here, if you did everything correctly, you can just run this code and should see the correct results
 
-    # importance
-    sns.heatmap(importances, ax=axs[1, 1])
-    axs[1, 1].set_title('Feature importances')
-    plt.show()
-    # Your code ends here _____________________________________________________________________________________
-
-    return fig
-
-fig = plot_heatmaps(x_train, y_train, model)
+n = 5       #number of samples
+d = 10      #number of features
+eps_list = [1e-1,1e-4,1e-11]     # epsilon values to test
+X_random, y_random, w_random = generate_random(n,10,RSEED)
+analytical_gradient = logistic_gradient(w_random,X_random,y_random)
+num_gradients = numerical_gradient(w_random,X_random,y_random,eps_list,cost)
+comparison_results=[]
+for grad in num_gradients:
+    comparison_results.append(comparison(analytical_gradient, grad))
+# Check outputs
+assert len(comparison_results) == len(eps_list), "List with comparison results should be the same length as there are epsilon values."
+assert isinstance(comparison_results[0],bool), f"Comparison results should be list of booleans."
+print("Your randomly generated data:\n")
+print("X =",X_random,"\n")
+print("y =",y_random,"\n")
+print("w = ",w_random,"\n")
+print("Logistic gradient:\n",analytical_gradient,"\n")
+for i,res in enumerate(comparison_results):
+    eps_ = eps_list[i]
+    print(f"Numerical gradient {i} with epsilon = {eps_}:\n", num_gradients[i], "\n")
+    print("    Vectors within absolute tolerance of 10^-7: ",res, "\n")
