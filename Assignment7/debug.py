387f5f1c6bbb76050f7ec9c5167f92a5814a645e
shaslhofer <haslhofer@unisoftwareplus.com>
2024-01-14 17:01:01 +0100

    assignment 7 task 2.1

--- Assignment7/debug.py
+++ Assignment7/debug.py
@@ -1,176 +1,69 @@
+# Python Implementation
 import numpy as np
-from sklearn.metrics import roc_curve, auc
-from sklearn.utils import shuffle
-import matplotlib
-import matplotlib.pyplot as plt
-from typing import Tuple
 
-# set the seed for reproducability to 44
-RSEED = 44
-np.random.seed(RSEED)
+# Notes on shapes of matrices:
+# the pre-activations and activations should be row-vectors
+# meaning shape = (1, ...). Think of it as having a mini-batch size of 1
+# Check out the pytorch docs on the linear layer for the forward pass and the shape
+# of the weight-layers ;)
+
+input = np.array([1, 1.5]).reshape(1, -1)
+
+weights_layer1 = ...  # fill in the correct values
+weights_layer2 = ...  # fill in the correct values
+
+assert weights_layer1.shape == (3, 2)
+assert weights_layer2.shape == (1, 3)
 
 
-def logistic_gradient(w: np.ndarray, x: np.ndarray, y: np.ndarray) -> np.ndarray:
-    """Function that computes the logistic gradient via the cross-entropy loss.
+# Note: Every function can be easily written as a small one-liner
+# If you have bigger functions, maybe think of a better solution :)
 
-    Parameters
-    ----------
-    w : np.ndarray
-        weights
-    x : np.ndarray
-        data matrix
-    y : np.ndarray
-        data labels
+def relu(x):
+    """calculate relu activation of x"""
+    return ...
 
-    Returns
-    -------
-    np.ndarray
-        gradient vector
+
+def derivative_relu(x):
+    """calculate derivative of relu for input x"""
+    return ...
+
+
+def calc_activation(preactivation):
+    """calculate activation given pre-activation
     """
-    gradient = np.ndarray(shape=np.shape(w))
-
-    for j in range(len(w)):
-        for i in range(len(y)):
-            sigma_i = 1 / (1 + np.exp(-(np.dot(w, x[i]))))
-            gradient[j] += (sigma_i - y[i]) * x[i][j]
-
-    return gradient
+    return ...
 
 
-def generate_random(nr_samples: int, nr_features: int, seed=RSEED) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
-    """Function that generates a random matrix X and the random vectors y and weights according to input specifications.
-    Hint: to generate the distributions use numpy's np.random.<...> functions.
+def calc_preactivation(x, w):
+    """calculate preactivation of a linear layer
 
-    Parameters
-    ----------
-    nr_samples : int,
-        number of samples to generate
-    nr_features : int,
-        number of features of each sample
-    seed: int, optional
-        seed for np.random, dont change it!
-    Returns
-    -------
-    tuple(np.ndarray,np.ndarray,np.ndarray)
-        returns randomly generated data matrices X,y and w.
+    x: layer inputs
+    w: layer weights
     """
-    # don't change the seed!
-    np.random.seed(seed)
-
-    # Your code goes here ↓↓↓
-    X_random = np.random.normal(size=(nr_samples, nr_features))
-    y_random = np.random.randint(size=(nr_samples), low=0, high=2)
-    w_random = np.random.normal(size=(nr_features))
-
-    return (X_random, y_random, w_random)
+    return ...
 
 
-def numerical_gradient(w: np.ndarray, x: np.ndarray, y: np.ndarray, eps_list: list, cost_function: callable) -> list:
-    """Function that computes the numerical gradient for each epsilon in eps_list.
-    Hint: Use the previously implemented cost function, it is given as input to this function,
-    make sure to call it with the correct name (cost_function(...), not cost(...))!
+def calc_delta_6(y_hat, y):
+    """delta error of last layer"""
+    return ...
 
-    Parameters
-    ----------
-    w : np.ndarray
-        weights
-    x : np.ndarray
-        data matrix
-    y : np.ndarray
-        data labels
-    eps : list
-        list of floats; the epsilon values for difference quotient calculation
-    cost_function: callable
-        function that calculates the cross-entropy loss, implemented by you
 
-    Returns
-    -------
-    list
-        list of same length as eps_list containing the np.ndarrays from the computed gradients for each epsilon in eps_list
+def calc_delta(s, delta_6, w):
+    """delta error of non-last layer
+
+    s: correct preactivation
+    delta_6: delta error of last layer
+    w: correct nn weight
+
+    to get correct inputs check the formulas and their indices ;)
     """
-    # Your code goes here ↓↓↓
-    dw_list = []
-    # Unit 5;p. 41
-    for idx, eps in enumerate(eps_list):
-        dw_list.append(np.zeros(shape=np.shape(w)))
-        for i in range(len(w)):
-            # p. 41
-            e_i = np.zeros(len(w))
-            e_i[i] = 1
-            dw_list[idx][i] = (cost_function(w + eps * e_i, x, y) - cost_function(w - eps * e_i, x, y))/(2*eps)
-
-    return dw_list
+    return ...
 
 
-def cost(w: np.ndarray, x: np.ndarray, y: np.ndarray) -> float:
-    """Function that computes the cross-entropy loss
-
-    Parameters
-    ----------
-    w : np.ndarray
-        weights
-    x : np.ndarray
-        data matrix
-    y : np.ndarray
-        data labels
-
-    Returns
-    -------
-    float
-        cross-entropy loss
+def calc_derivative_L(delta_error, activation):
+    """calc derivative of loss-fct w.r.t a certain weight.
+    check formulas to now what delta_error and activation you have to
+    provide :)
     """
-    # Your code goes here ↓↓↓
-    loss = 0
-
-    for i in range(len(y)):
-        sigma = 1 / (1 + np.exp(-(np.dot(w, x[i]))))
-        loss += y[i] * np.log(sigma) + (1 - y[i]) * np.log(1 - sigma)
-
-    return -loss
-
-
-def comparison(grad_a: np.ndarray, grad_n: np.ndarray) -> bool:
-    """Function that compares two arrays, in our case the numerical and analytical gradients.
-    Hint: use absolute tolerance, with an error tolerance of 1e-7.
-
-    Parameters
-    ----------
-    grad_a : np.ndarray
-        the analytical gradient
-    grad_n : np.ndarray
-        the numberical gradient
-
-    Returns
-    -------
-    bool
-        True, if arrays are close, according to our specifications, False if not.
-    """
-    close = np.all(np.abs(grad_a - grad_n) <= 1e-7)
-
-    return bool(close)
-
-
-# Nothing to do here, if you did everything correctly, you can just run this code and should see the correct results
-
-n = 5  # number of samples
-d = 10  # number of features
-eps_list = [1e-1, 1e-4, 1e-11]  # epsilon values to test
-X_random, y_random, w_random = generate_random(n, 10, RSEED)
-analytical_gradient = logistic_gradient(w_random, X_random, y_random)
-num_gradients = numerical_gradient(w_random, X_random, y_random, eps_list, cost)
-comparison_results = []
-for grad in num_gradients:
-    comparison_results.append(comparison(analytical_gradient, grad))
-# Check outputs
-assert len(comparison_results) == len(
-    eps_list), "List with comparison results should be the same length as there are epsilon values."
-assert isinstance(comparison_results[0], bool), f"Comparison results should be list of booleans."
-print("Your randomly generated data:\n")
-print("X =", X_random, "\n")
-print("y =", y_random, "\n")
-print("w = ", w_random, "\n")
-print("Logistic gradient:\n", analytical_gradient, "\n")
-for i, res in enumerate(comparison_results):
-    eps_ = eps_list[i]
-    print(f"Numerical gradient {i} with epsilon = {eps_}:\n", num_gradients[i], "\n")
-    print("    Vectors within absolute tolerance of 10^-7: ", res, "\n")
+    return ...
