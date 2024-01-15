be1bc4235a31e019cd475e5d4a77c80a264eac38
StefanHaslhofer <haslhofer.stefan@gmail.com>
2024-01-16 00:24:51 +0100

    finish assignment 7

--- Assignment7/debug.py
+++ Assignment7/debug.py
@@ -10,84 +10,168 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-# Notes on shapes of matrices:
-# the pre-activations and activations should be row-vectors
-# meaning shape = (1, ...). Think of it as having a mini-batch size of 1
-# Check out the pytorch docs on the linear layer for the forward pass and the shape
-# of the weight-layers ;)
+class ANN_sigmoid(nn.Module):
+    def __init__(self, D_in, D_out):
+        super(ANN_sigmoid, self).__init__()
+        self.linear1 = nn.Linear(D_in, 512)
+        self.linear2 = nn.Linear(512, 1024)
+        self.linear3 = nn.Linear(1024, 512)
+        self.linear4 = nn.Linear(512, D_out)
 
-input = np.array([1, 1.5]).reshape(1, -1)
+    def forward(self, x):
+        x = self.linear1(x)
+        x = torch.sigmoid(x)
+        x = self.linear2(x)
+        x = torch.sigmoid(x)
+        x = self.linear3(x)
+        x = torch.sigmoid(x)
+        x = self.linear4(x)
+        return F.log_softmax(x, dim=1)
 
-weights_layer1 = np.array([[-0.1, 0.5], [-1, 0], [0.1, -2]])  # fill in the correct values
-weights_layer2 = np.array([[1.2, -1, 0.5]])  # fill in the correct values
+class ANN_relu(nn.Module):
+    # Your code for ReLU NN
 
-assert weights_layer1.shape == (3, 2)
-assert weights_layer2.shape == (1, 3)
+    ## Solution start
+    def __init__(self, D_in, D_out):
+        super(ANN_relu, self).__init__()
+        self.linear1 = nn.Linear(D_in, 512)
+        self.linear2 = nn.Linear(512, 1024)
+        self.linear3 = nn.Linear(1024, 512)
+        self.linear4 = nn.Linear(512, D_out)
+
+    def forward(self, x):
+        x = self.linear1(x)
+        x = torch.relu(x)
+        x = self.linear2(x)
+        x = torch.relu(x)
+        x = self.linear3(x)
+        x = torch.relu(x)
+        x = self.linear4(x)
+        ## Solution end
+
+        return F.log_softmax(x, dim=1)
+
+# Here we collect the hyperparameters we are going to use
+args = SimpleNamespace(batch_size=64, test_batch_size=1000, epochs=5,
+                       lr=0.01, momentum=0.5, seed=1, log_interval=100)
+torch.manual_seed(args.seed)
+use_cuda = torch.cuda.is_available()
+device = torch.device('cuda' if use_cuda else 'cpu')
+
+# Data loader (downloads data automatically the first time)
+# 0.1307 and 0.3081 are the mean and the std computed on the training set
+kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+train_loader = torch.utils.data.DataLoader(
+    datasets.FashionMNIST(os.path.join('.','..','data'), train=True, download=True,
+                   transform=transforms.Compose([
+                       transforms.ToTensor(),
+                       transforms.Normalize((0.1307,), (0.3081,))
+                   ])),
+    batch_size=args.batch_size, shuffle=True, **kwargs)
+test_loader = torch.utils.data.DataLoader(
+    datasets.FashionMNIST(os.path.join('.','..','data'), train=False, transform=transforms.Compose([
+                       transforms.ToTensor(),
+                       transforms.Normalize((0.1307,), (0.3081,))
+                   ])),
+        batch_size=args.test_batch_size, shuffle=True, **kwargs)
+
+print(train_loader.dataset.targets.shape)
+print(train_loader.dataset.data.shape)
+input_dim = train_loader.dataset.data.shape[1]*train_loader.dataset.data.shape[2]
+print("Input dimension is {}.".format(input_dim))
+output_dim = 10
+
+# Plot example images
+fig=plt.figure(figsize=(15,3))
+for image in range(20):
+    show_img = train_loader.dataset.data[image].numpy().reshape(28, 28)
+    fig.add_subplot(2,10,image+1)
+    plt.xticks([]),plt.yticks([])
+    plt.imshow(show_img, cmap='gray')
+plt.show()
+
+# This function trains the model for one epoch
+# Nothing to do here
+def train(args, model, device, train_loader, optimizer, epoch):
+    model.train()
+    grads1_list = []
+    grads2_list = []
+    grads3_list = []
+    correct = 0
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data = Variable(data.view(-1, input_dim))
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+
+        # This part of the code gets the weights in the different layers
+        grads3 = abs(model.linear3.weight.grad)
+        grads2 = abs(model.linear2.weight.grad)
+        grads1 = abs(model.linear1.weight.grad)
+        grads3_list.append(torch.mean(grads3).item())
+        grads2_list.append(torch.mean(grads2).item())
+        grads1_list.append(torch.mean(grads1).item())
+
+        optimizer.step()
+        if batch_idx % args.log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                       100. * batch_idx / len(train_loader), loss.item()))
+        pred = output.max(1, keepdim=True)[1]  # get the index of the max log-probability
+        correct += pred.eq(target.view_as(pred)).sum().item()
+    print('\nTraining set: Accuracy: {}/{} ({:.2f}%)\n'.format(
+        correct, len(train_loader.dataset),
+        100. * correct / len(train_loader.dataset)))
+    return grads1_list, grads2_list, grads3_list
+
+# This function evaluates the model on the test data
+def test(args, model, device, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data = Variable(data.view(-1, input_dim))
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.nll_loss(output, target, reduction='sum').item() # sum up batch loss
+            pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+    test_loss /= len(test_loader.dataset)
+
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
 
 
-# Note: Every function can be easily written as a small one-liner
-# If you have bigger functions, maybe think of a better solution :)
+print("<<< Sigmoid >>>\n")
+model = ANN_sigmoid(input_dim, output_dim).to(device)
 
-def relu(x):
-    """calculate relu activation of x"""
-    return np.maximum(0, x)
-
-
-def derivative_relu(x):
-    """calculate derivative of relu for input x"""
-    return 0 if x < 0 else 1 if x > 0 else None
+# please use only predefined variables names
+optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)  # define SGD optimizer
+epochs_range = range(args.epochs)  # range you will iterate over
 
 
-def calc_activation(preactivation):
-    """calculate activation given pre-activation
-    """
-    return relu(preactivation)
 
+    # now test your model
 
-def calc_preactivation(x, w):
-    """calculate preactivation of a linear layer
+######################################################################################
+print("<<< ReLU >>>\n")
+model = ANN_relu(input_dim, output_dim).to(device) #define your ReLU model
+optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum) #redefine SGD optimizer
 
-    x: layer inputs
-    w: layer weights
-    """
-    return np.sum(x * w)
+for epoch in epochs_range:
+    # repeat operations of the loop above, but for ReLU model
+    grads1_list, grads2_list, grads3_list = train(args, model, device, train_loader, optimizer, epoch)
 
-
-def calc_delta_6(y_hat, y):
-    """delta error of last layer"""
-    return y_hat - y
-
-
-def calc_delta(s, delta_6, w):
-    """delta error of non-last layer
-
-    s: correct preactivation
-    delta_6: delta error of last layer
-    w: correct nn weight
-
-    to get correct inputs check the formulas and their indices ;)
-    """
-    return derivative_relu(s) * delta_6 * w
-
-
-def calc_derivative_L(delta_error, activation):
-    """calc derivative of loss-fct w.r.t a certain weight.
-    check formulas to now what delta_error and activation you have to
-    provide :)
-    """
-    return delta_error * activation
-
-
-x1 = 1
-x2 = 1.5
-w31 = -0.1
-w41 = -1
-w51 = 0.1
-w32 = 0.5
-w42 = 0
-w52 = -2
-w63 = 1.2
-w64 = -1
-w65 = 0.5
-
-# TODO test
+    plt.figure(figsize=(16, 9))
+    plt.title('ReLU model')
+    plt.xlabel('Iterations')
+    plt.ylabel('Means of gradient magnitudes')
+    plt.plot(range(len(grads1_list)), grads1_list, label='layer1')
+    plt.plot(range(len(grads2_list)), grads2_list, label='layer2')
+    plt.plot(range(len(grads3_list)), grads3_list, label='layer3')
+    plt.legend()
+    plt.show()
